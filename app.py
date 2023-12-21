2d26a09eae870968a3acfc45391100578fe5fa22
abdelkader <abdelkaderboukart@gmail.com>
2023-12-21 13:54:07 +0100

    m

---
+++ app.py
@@ -0,0 +1,145 @@
+from flask import Flask, render_template, redirect, url_for, flash, request, jsonify, json
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String
+from waitress import serve
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError, IntegerField, DateField, SelectField
+from wtforms.validators import DataRequired, equal_to, Length
+from flask_wtf.csrf import CSRFProtect, generate_csrf, validate_csrf
+from flask_bcrypt import Bcrypt  # Import Bcrypt
+from flask_login import UserMixin, login_user, login_manager, logout_user, login_required, current_user, login_remembered,LoginManager
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime, timedelta
+import datetime
+from wtforms.widgets import TextArea
+from flask_apscheduler import APScheduler
+from flask_migrate import Migrate
+from jinja2_time import TimeExtension
+from validate_email import validate_email
+
+app = Flask(__name__)
+#csrf = CSRFProtect(app)
+REMEMBER_COOKIE_DURATION = timedelta(days=0)
+app.config['SECRET_KEY'] = '5511467d654732b6d9875da2691f78fd'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)  # Initialize Bcrypt
+# flask_login stuff
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+scheduler = APScheduler()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class userForm(FlaskForm):
+    name = StringField("name", validators=[DataRequired()])
+    email = StringField("email", validators=[DataRequired()])
+    password_hash = PasswordField("password", validators=[DataRequired(), equal_to('password_hash2', message='password must match!')])
+    password_hash2 = PasswordField("password", validators=[DataRequired()]) 
+    submit = SubmitField('creat a new account')
+
+class loginForm(FlaskForm):
+    email = StringField("email", validators=[DataRequired()])
+    password = PasswordField("password", validators=[DataRequired()]) 
+    remember = BooleanField('Remember me')
+    submit = SubmitField('login')  
+
+app.jinja_env.filters['datetime'] = datetime.datetime.strptime
+#the module
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
+    post = db.relationship('to_do', backref='todos')  
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable')
+    
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<name %r>' % self.name
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    form=loginForm()
+    for i in enumerate(form):
+        print(i[1].data)
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        passed = check_password_hash(user.password_hash, form.password.data )
+        if user is None or not passed :
+            form.password.data = ''
+            form.email.data = ''
+            flash(" this account does not exist")
+            return render_template('login.html', form = form)
+        else :
+            if passed is True :
+                b = form.remember.data
+                flash("login successful")
+                login_user(user, remember=b)
+                return redirect(url_for('home'))
+            else :
+                flash("wrong password try again")
+                form.password.data = ''
+                return render_template('login.html', form = form)
+        
+    else :
+      return render_template('login.html', form = form)
+    
+@app.route('/add_user', methods = ['POST' , 'GET'])
+def add_user():
+    form = userForm()
+    if form.validate_on_submit():
+        v = validate_email(form.email.data, verify=True)
+        if v:
+          user = User.query.filter_by(email=form.email.data).first()
+          print('''user: {user}''')
+          if user is None:
+              #hash password!!
+              hashed_pw = generate_password_hash(form.password_hash.data)
+              user = User(name=form.name.data, email=form.email.data, password_hash=hashed_pw)
+              db.session.add(user)
+              db.session.commit()
+
+              flash("your account has been created please login ")
+              return redirect(url_for('login'))
+          else :
+              flash("this accont already exist please try to login")
+              return redirect(url_for('login'))     
+        else:
+           form.email.data = ''
+           return render_template('add_user.html',
+                               form = form,
+                               ) 
+    else :
+        return render_template('add_user.html',
+                               form = form,
+                               )
+
+@app.route('/logout', methods = ['GET', 'POST'])
+@login_required
+def log_out():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+with app.app_context():
+        db.create_all()
+
+if __name__ == "__main__":
+
+
+    #migrate = Migrate(app, db)
+    scheduler.start()
+    serve(app, host='0.0.0.0', port=8080)
